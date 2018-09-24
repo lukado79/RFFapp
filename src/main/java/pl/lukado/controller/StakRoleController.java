dc10c599ae5855e04a81670f1aedd1086e851576
Łukasz Izdebski <lukaszizdebski@gazeta.pl>
2018-09-25 00:57:54 +0200

    security for stakeholder role controller

--- src/main/java/pl/lukado/controller/StakRoleController.java
+++ src/main/java/pl/lukado/controller/StakRoleController.java
@@ -1,5 +1,6 @@
 package pl.lukado.controller;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pl.lukado.entity.StakRole;
+import pl.lukado.entity.User;
 import pl.lukado.service.StakRoleService;
 
 @Controller
@@ -22,35 +24,117 @@ public class StakRoleController {
 	StakRoleService stakRoleService;
 
 	@GetMapping("/add")
-	public String addStakRole(Model model) {
-		return stakRoleService.addStakRole(model);
+	public String addStakRole(Model model, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName())) {
+				return stakRoleService.addStakRole(model);
+			} else if ("user".equals(user.getRole().getRoleName())) {
+				return "accessView";
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
 
 	}
 
 	@PostMapping("/add")
-	public String addStakRole(@Valid @ModelAttribute StakRole stakRole, BindingResult result) {
-		return stakRoleService.addStakRole(stakRole, result);
+	public String addStakRole(@Valid @ModelAttribute StakRole stakRole, BindingResult result, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName())) {
+				return stakRoleService.addStakRole(stakRole, result);
+			} else if ("user".equals(user.getRole().getRoleName())) {
+				return "accessView";
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
+
 	}
 
 	@GetMapping("/all")
-	public String allStakRole(Model model) {
-		return stakRoleService.allStakRole(model);
+	public String allStakRole(Model model, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName())) {
+				return stakRoleService.allStakRole(model);
+			} else if ("user".equals(user.getRole().getRoleName())) {
+				return "accessView";
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
+
 	}
 
 	@GetMapping("/delete/{id}")
-	public String deleteStakRole(@PathVariable long id) {
-		return stakRoleService.deleteStakRole(id);
+	public String deleteStakRole(@PathVariable long id, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName())) {
+				return stakRoleService.deleteStakRole(id);
+			} else if ("user".equals(user.getRole().getRoleName())) {
+				return "accessView";
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
 
 	}
 
 	@GetMapping("/edit/{id}")
-	public String editStakRole(Model model, @PathVariable long id) {
-		return stakRoleService.editStakRole(model, id);
+	public String editStakRole(Model model, @PathVariable long id, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName())) {
+				return stakRoleService.editStakRole(model, id);
+			} else if ("user".equals(user.getRole().getRoleName())) {
+				return "accessView";
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
+
 	}
 
 	@PostMapping("/edit/{id}")
-	public String editStakRole(@Valid @ModelAttribute StakRole stakRole, BindingResult result) {
-		return stakRoleService.editStakRole(stakRole, result);
+	public String editStakRole(@Valid @ModelAttribute StakRole stakRole, BindingResult result, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName())) {
+				return stakRoleService.editStakRole(stakRole, result);
+			} else if ("user".equals(user.getRole().getRoleName())) {
+				return "accessView";
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
+		
 	}
 
 }
