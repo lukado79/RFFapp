f50fb70d7d63d3d02b1ce8bcea88008b2d28d55a
Łukasz Izdebski <lukaszizdebski@gazeta.pl>
2018-09-25 00:50:13 +0200

    security for stakeholder controller

--- src/main/java/pl/lukado/controller/StakeholderController.java
+++ src/main/java/pl/lukado/controller/StakeholderController.java
@@ -2,6 +2,7 @@ package pl.lukado.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import pl.lukado.entity.StakRole;
 import pl.lukado.entity.Stakeholder;
+import pl.lukado.entity.User;
 import pl.lukado.service.StakeholderService;
 
 @Controller
@@ -25,35 +27,109 @@ public class StakeholderController {
 	StakeholderService stakeholderService;
 
 	@GetMapping("/add")
-	public String addStakeholder(Model model) {
-		return stakeholderService.addStakeholder(model);
+	public String addStakeholder(Model model, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName()) || "user".equals(user.getRole().getRoleName())) {
+				return stakeholderService.addStakeholder(model);
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
 
 	}
 
 	@PostMapping("/add")
-	public String addStakeholder(@Valid @ModelAttribute Stakeholder stakeholder, BindingResult result) {
-		return stakeholderService.addStakeholder(stakeholder, result);
+	public String addStakeholder(@Valid @ModelAttribute Stakeholder stakeholder, BindingResult result,
+			HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName()) || "user".equals(user.getRole().getRoleName())) {
+				return stakeholderService.addStakeholder(stakeholder, result);
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
+
 	}
 
 	@GetMapping("/all")
-	public String allStakeholder(Model model) {
-		return stakeholderService.allStakeholder(model);
+	public String allStakeholder(Model model, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName()) || "user".equals(user.getRole().getRoleName())) {
+				return stakeholderService.allStakeholder(model);
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
+
 	}
 
 	@GetMapping("/delete/{id}")
-	public String deleteStakeholder(@PathVariable long id) {
-		return stakeholderService.deleteStakeholder(id);
+	public String deleteStakeholder(@PathVariable long id, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName())) {
+				return stakeholderService.deleteStakeholder(id);
+			} else if ("user".equals(user.getRole().getRoleName())) {
+				return "accessView";
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
 
 	}
 
 	@GetMapping("/edit/{id}")
-	public String editStakeholder(Model model, @PathVariable long id) {
-		return stakeholderService.editStakeholder(model, id);
+	public String editStakeholder(Model model, @PathVariable long id, HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName()) || "user".equals(user.getRole().getRoleName())) {
+				return stakeholderService.editStakeholder(model, id);
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
+
 	}
 
 	@PostMapping("/edit/{id}")
-	public String editStakeholder(@Valid @ModelAttribute Stakeholder stakeholder, BindingResult result) {
-		return stakeholderService.editStakeholder(stakeholder, result);
+	public String editStakeholder(@Valid @ModelAttribute Stakeholder stakeholder, BindingResult result,
+			HttpSession session) {
+
+		User user;
+		user = (User) session.getAttribute("user");
+		try {
+			if ("admin".equals(user.getRole().getRoleName()) || "user".equals(user.getRole().getRoleName())) {
+				return stakeholderService.editStakeholder(stakeholder, result);
+			} else {
+				return "wrongLogin";
+			}
+		} catch (NullPointerException e) {
+			return "wrongData";
+		}
+
 	}
 
 	@ModelAttribute("stakRole")
